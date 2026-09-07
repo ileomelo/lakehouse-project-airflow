38d81aac8d1ab8eff2df7a9f5eb62e84a8761e94
Leonardo Melo <leonardo.mel04@outlook.com>
2026-09-07 13:38:33 -0300

    refactor(crm): separate extract and load tasks for customer info

--- dags/crm_bronze.py
+++ dags/crm_bronze.py
@@ -9,21 +9,43 @@ with DAG(
 ) as dag:
 
     @task
-    def extract_and_load_cust_info():
+    def extract_cust_info():
 
         input_path = Path("/opt/airflow/data/source/crm/cust_info.csv")
 
-        output_path = Path("/opt/airflow/data/bronze/crm/cust_info/cust_info.parquet")
+        staging_path = Path("/opt/airflow/data/bronze/crm/cust_info/cust_info.parquet")
 
-        output_path.parent.mkdir(
+        staging_path.parent.mkdir(
             parents=True,
             exist_ok=True,
         )
 
         df = pd.read_csv(input_path)
 
-        df.to_parquet(output_path)
+        df.to_parquet(staging_path)
 
-        print(f"Bronze criada em: {output_path}")
+        return str(staging_path)
 
-    extract_and_load_cust_info()
+    extract_cust_info()
+
+    @task
+    def load_cust_info(staging_path):
+
+        staging_path = Path(staging_path)
+
+        bronze_path = Path("/opt/airflow/data/bronze/crm/cust_info/cust_info.parquet")
+
+        bronze_path.parent.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+
+        df = pd.read_parquet(staging_path)
+
+        df.to_parquet(bronze_path)
+
+        print(f"Data loaded to bronze layer at: {bronze_path}")
+
+        staging_path = extract_cust_info()
+
+        load_cust_info(staging_path)
